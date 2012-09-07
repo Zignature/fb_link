4cf76a31f9f5676f5a8820f18ea5ea2c80de320b
Ronald Hickson <ron@hicksondesign.com>
2012-09-06 22:02:40 -0400

    Added typography formatting

--- system/expressionengine/third_party/fb_link/mod.fb_link.php
+++ system/expressionengine/third_party/fb_link/mod.fb_link.php
@@ -28,6 +28,11 @@ class Fb_link {
 			'fields'	=>	$this->EE->TMPL->fetch_param('fields'),
 		);
 		
+		/*
+		// LEGACY CODE
+		//
+		// This was necessary before a major update to the Graph API.  That made life easier..
+		//
 		if(empty($params['limit'])) {
 			unset($params['limit']);
 		}
@@ -35,6 +40,7 @@ class Fb_link {
 		if(empty($params['fields'])) {
 			unset($params['fields']);
 		}
+		*/
 		
 		// Set the path
 		$path = $params['graph'];
@@ -42,15 +48,15 @@ class Fb_link {
 		
 		try {
 			// We need to set the index for the parser later
-			$data = $this->EE->base_facebook->graph($path, $params);
+			$data = $this->EE->base_facebook->graph($path);
 		} catch (FacebookApiException $e) {
 			error_log($e);
 			return $output;
 		}
 				
 		// We need to make some "rows" for the EE parser.
-		$rows = $this->make_rows($data);
-		
+		$rows[] = $this->make_rows($data);
+
 		/*
 		//
 		// This may be handy for pagination later but for now it's just filed away.
@@ -60,13 +66,12 @@ class Fb_link {
 			$paging = $page_match[1];
 			// Replace the {paging} variable pairs with nothing and set this aside for later.
 			$tag_data = preg_replace("/".LD."paging".RD.".+?".LD.'\/'."paging".RD."/s", "", $this->EE->TMPL->tagdata);
-		} else {
-			$tag_data = $this->EE->TMPL->tagdata;
-		}
 		*/
+		
+		$tag_data = $this->EE->TMPL->tagdata;
 				
-		// Do some formatting for the data
-		foreach($rows['data'] as $item => $row) {
+		// Now we parse by row so we can do some formatting for the data and utilize conditionals.
+		/*foreach($rows as $item => $row) {
 				
 			// Format the message HTML is one exists				
 			if(isset($row['message'])) {
@@ -89,7 +94,9 @@ class Fb_link {
 			$tagdata = $this->EE->functions->prep_conditionals($tag_data, $cond);
 			$output .= $this->EE->TMPL->parse_variables_row($tagdata, $row);
 			
-		}
+		}*/
+		
+		$output = $this->EE->TMPL->parse_variables($tag_data, $rows);
 												
 		return $output;
 		
@@ -98,17 +105,24 @@ class Fb_link {
 	// Create rows for the EE parser.  Some FB data is an array that is not indexed.  For example the from data is an associative array.  THe EE parser needs a "row" to work with.  This function will recursively work through the data and if an array is not indexed will create the index.  It's a beast of a function but necessary for now and should be flexible enough to cope with FB structure changes.
 	public function make_rows($array) {
 		$var = array();
-				
+		
+		// Let work through each item to catch arrays and format them for parsing
 		foreach($array as $k => $v) {
+		
+			// Not an array so pass over to the formatting function.
 			if(!is_array($v)) {
-				$var[$k] = $v;
+				$var[$k] = $this->format($k,$v);
+				
+			// Is an array so hold up we need some more work.
 			} elseif(is_array($v)){
+				// If it's not numeric we need to create that.
+				
 				if(!is_numeric($k) && !is_numeric(array_shift(array_keys($v)))) {
 				
 					// We need to rename the "row" named data based on it's parent or else the parser gets confused.
 					if(isset($v['data'])) {
-						$v[$k.'_data'] = $v['data'];
-						unset($v['data']);
+						$v[$k.':data'] = $v['data'];
+						unset($v['data']);						
 					}
 					
 					$var[$k][0] = $this->make_rows($v);
@@ -121,6 +135,18 @@ class Fb_link {
 		return $var;
 	}
 
+	// Now we do the formatting of certain key fields
+	public function format($k, $v) {		
+		if($k == 'message') {
+			$v = auto_link($this->EE->typography->parse_type($v, array('text_format' => 'lite', 'html_format' => 'safe', 'auto_links' => 'y')));
+		}
+		
+		if(($k == 'created_time') || ($k == 'updated_time')) {
+			$v = strtotime($v);
+		}
+		
+		return $v;
+	}
 }
 
 /* End of file mod.fb_link.php */
